8836aade0b24bdf39c9af7d9ec8a2887aeefa684
DavidHompaneraCampos <davidhompaneracampos@gmail.com>
2024-11-30 14:21:31 +0100

    loginFront

--- registro.php
+++ registro.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro - ForoPro</title>
-    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="styles/registro.css">
     <link rel="icon" href="img/Ico Imagotipo.ico" type="image/x-icon">
 </head>
 <body>
